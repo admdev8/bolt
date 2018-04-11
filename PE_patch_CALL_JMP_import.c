c304790fe0b014a17173df98a0541b4ed803396b
Dennis Yurichev <dennis@yurichev.com>
2018-04-12 02:00:23 +0300

    ...

--- PE_patch_CALL_JMP_import.c
+++ PE_patch_CALL_JMP_import.c
@@ -6,7 +6,7 @@
  * | |_) | (_) | | |_ 
  * |_.__/ \___/|_|\__|
  *
- * Written by Dennis Yurichev <dennis(a)yurichev.com>, 2017
+ * Written by Dennis Yurichev <dennis(a)yurichev.com>, 2017-2018
  *
  * This work is licensed under the Creative Commons Attribution-NonCommercial-NoDerivs 3.0 Unported License. 
  * To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/.
@@ -33,36 +33,35 @@ address import_adr;
 
 struct my_CB_data
 {
-	bool found;
-	address adr;
+	obj*	lst;
+	bool	find_JMP;
 };
 
-bool is_it_FF_15_CALL (address a, struct Da* d, void* void_data)
+bool is_it_JMP_CALL (address a, struct Da* d, void* void_data)
 {
 	struct my_CB_data *data=(struct my_CB_data*)void_data;
 
-	if (d->ins_code==I_CALL && Da_1st_op_is_disp_only(d))
+	if (
+		((data->find_JMP==false && d->ins_code==I_CALL) ||
+		(data->find_JMP && d->ins_code==I_JMP))
+		&& Da_1st_op_is_disp_only(d))
 	{
 		uint64_t tmp=Da_1st_op_get_disp(d);
 		if (tmp==import_adr)
-		{
-			data->found=true;
-			data->adr=a;
-			return false;
-		};
+			data->lst=add_to_list(data->lst, obj_octa(a));
 	}
 
-	return true; // continue
+	return true; // continue anyway
 };
 
-obj* find_CALL_to_patch(LOADED_IMAGE *im, IMAGE_SECTION_HEADER* text_sect)
+obj* find_CALL_JMP_to_patch(LOADED_IMAGE *im, IMAGE_SECTION_HEADER* text_sect, bool find_JMP)
 {
-	obj* lst=NULL;
+	const char *ins=find_JMP ? "JMP" : "CALL";
 
 	struct my_CB_data CB_data;
 	bzero (&CB_data, sizeof(struct my_CB_data));
 
-	printf ("%s() disasming...\n", __FUNCTION__);
+	printf ("%s() disasming... searching for %s's\n", __FUNCTION__, ins);
 
 	IMAGE_SECTION_HEADER* pdata_sect=PE_find_section_by_name (im, ".pdata");
 
@@ -73,36 +72,46 @@ obj* find_CALL_to_patch(LOADED_IMAGE *im, IMAGE_SECTION_HEADER* text_sect)
 			(struct RUNTIME_FUNCTION*)(im->MappedAddress + pdata_sect->PointerToRawData);
 			p->FunctionStart!=0 && p->FunctionEnd!=0; p++)
 	{
-		PE_disasm_range(im, text_sect, p->FunctionStart, p->FunctionEnd-p->FunctionStart, Fuzzy_True, is_it_FF_15_CALL, &CB_data, /* report_error */ false);
-
-		if (CB_data.found)
+		CB_data.find_JMP=find_JMP;
+		if (p->FunctionEnd < p->FunctionStart)
 		{
-			printf ("%s() CALL to be patched found at 0x%" PRIx64 " (VA)\n", __FUNCTION__, PE_get_original_base(im)+CB_data.adr);
-			
-			lst=add_to_list(lst, obj_octa(CB_data.adr));
-			CB_data.found=false;
+			printf ("garbage in .pdata...\n");
+			break;
 		};
+		//printf ("p->FunctionStart=0x%x, p->FunctionEnd=0x%x\n", p->FunctionStart, p->FunctionEnd);
+		//printf ("running disasm. start=0x%x size=0x%x\n", p->FunctionStart, p->FunctionEnd - p->FunctionStart);
+		PE_disasm_range(im, text_sect, p->FunctionStart, p->FunctionEnd-p->FunctionStart, Fuzzy_True, is_it_JMP_CALL, &CB_data, /* report_error */ false);
 	};
 
-	if (LENGTH(lst)==0)
-		die ("%s() no CALLs found\n", __FUNCTION__);
+	if (LENGTH(CB_data.lst)==0)
+	{
+		printf ("%s() no %s's found\n", __FUNCTION__, ins);
+		return NULL;
+	}
 
-	return lst;
+	return CB_data.lst;
 };
 
-void patch_MOV (byte *ptr, int val)
+void put_MOV (address adr, byte *ptr, int val, bool is_JMP)
 {
-	if (ptr[0]==0xFF && ptr[1]==0x15)
+	const char *ins=is_JMP ? "JMP" : "CALL";
+
+	if (ptr[0]==0xFF && 
+		((is_JMP && ptr[1]==0x25) ||
+		(is_JMP==false && ptr[1]==0x15)))
 	{
 		ptr[0]=0xB8; // MOV EAX, imm32
 		memcpy (ptr+1, &val, sizeof(int));
 		ptr[5]=0x90; // NOP
+		if (is_JMP)
+			ptr[6]=0xC3; // RET
+		printf ("%s() %s patched at 0x%" PRIx64 " (VA)\n", __FUNCTION__, ins, adr);
 	}
 	else
-		printf ("Error: not a FF 15 CALL here.\n");
+		printf ("Error: not a FF x5 %s here.\n", ins);
 };
 
-#define PNAME "PE_remove_import"
+#define PNAME "PE_patch_CALL_JMP_import"
 
 int main(int argc, char* argv[])
 {
@@ -114,37 +123,63 @@ int main(int argc, char* argv[])
 		printf ("for example:\n");
 		printf ("\t%s filename.exe kernel32.dll WriteFile 1234\n", PNAME);
 		printf ("\t... and all CALLs to kernel32.dll!WriteFile will be replaced by MOV EAX, 1234\n");
+		printf ("\t... all JMPs to kernel32.dll!WriteFile will be replaced by MOV EAX, 1234 / RETN\n");
 		printf ("\t%s filename.exe advapi32.dll CryptVerifySignatureA 1\n", PNAME);
 		printf ("\t... as if CryptVerifySignatureA() would always return 1 (TRUE)\n");
-		printf ("\t... however, only FF 15 CALL instructions are replaced\n");
+		printf ("\t... however, only FF 15 CALL / FF 25 JMP instructions are handled\n");
 		return 0;
 	};
 	
 	int val;
 	if (sscanf(argv[4], "%d", &val)!=1)
 		die ("Can't parse [%s], must be a number\n", argv[3]);
-	
+
 	LOADED_IMAGE im;
 
 	MapAndLoad_or_die (argv[1], NULL, &im, false, /* ReadOnly */ false);
 
 	struct PE_get_imports_info *imports=PE_get_imports (&im);
 	import_adr=PE_find_thunk_by_import (imports, argv[2], argv[3]);
-	
+
 	IMAGE_SECTION_HEADER* text_sect=PE_find_section_by_name (&im, ".text");
 	if (text_sect==NULL)
 		die ("%s() section %s not found\n", __FUNCTION__, ".text");
 
-	obj *calls=find_CALL_to_patch(&im, text_sect);
+	// TODO join these parts:
 
-	// enumerate CALLs we've found:
-	for (obj* c=calls; c; c=cdr(c))
+	// ---- CALLs
+
+	obj *calls=find_CALL_JMP_to_patch(&im, text_sect, false);
+
+	if (calls)
 	{
-		byte *ptr=PE_section_get_ptr_in(&im, text_sect, obj_get_as_octa(c->u.c->head));
-		patch_MOV (ptr, val);
+		// enumerate CALLs we've found:
+		for (obj* c=calls; c; c=cdr(c))
+		{
+			byte *ptr=PE_section_get_ptr_in(&im, text_sect, obj_get_as_octa(c->u.c->head));
+			put_MOV (PE_get_original_base(&im) + obj_get_as_octa(c->u.c->head), ptr, val, false);
+		};
 	};
 
 	obj_free(calls);
+
+	// ---- JMPs
+
+	obj *jmps=find_CALL_JMP_to_patch(&im, text_sect, true);
+
+	if (jmps)
+	{
+		// enumerate CALLs we've found:
+		for (obj* c=jmps; c; c=cdr(c))
+		{
+			byte *ptr=PE_section_get_ptr_in(&im, text_sect, obj_get_as_octa(c->u.c->head));
+			put_MOV (PE_get_original_base(&im) + obj_get_as_octa(c->u.c->head), ptr, val, true);
+		};
+	};
+
+	obj_free(jmps);
+
+	// ----
 
 	PE_get_imports_info_free(imports);
 
